74a60c841da4c4b773e19fc4905febe56e1d90b1
vitali <v.kostev@gmail.com>
2019-09-24 15:53:48 +0300

    Dropdown on UserMenu

--- src/userMenu.js
+++ src/userMenu.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+    function openMenu() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn') && !event.target.matches('.UserMenu__profile_img')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+    
+
 class UserMenu extends React.Component{
     render(){
         return(
@@ -7,10 +25,16 @@ class UserMenu extends React.Component{
                 <i id="UserMenu__Bell" class="fa fa-bell fa-2x" aria-hidden="true"><div id="circle">2</div></i>
 
                 <div className="UserMenu__profile">
-                    <img alt="user_img" src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"/>
-                    <p>Maria Zvaginceva</p> 
-                </div>
-                                                      
+                    <img class="UserMenu__profile_img" onMouseDown={() => openMenu()}  alt="user_img" src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"/>
+                    <div class="dropdown">
+                        <button onMouseDown={() => openMenu()} class="dropbtn">Maria Zvaginceva</button>
+                        <div id="myDropdown" class="dropdown-content">
+                                <a href="#home">Home</a>
+                                <a href="#about">About</a>
+                                <a href="#contact">Contact</a>
+                        </div>
+                    </div>
+                </div>                      
             </div>
         )
     }    
