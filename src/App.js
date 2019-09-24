323b7ee1e1c0c13759293d6e3671badaf5f03b20
vitali <v.kostev@gmail.com>
2019-09-24 12:47:52 +0300

    Grid

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Header from "./header"
 import Footer from "./footer"
 import SideMenu from "./sideMenu"
+import Content from "./content.js"
 
 function App() {
   return (
-    <div>
+    <div className="main_container">
         <Header />
         <SideMenu />
+        <Content />
         <Footer />
     </div>
   );
