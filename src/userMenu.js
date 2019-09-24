fdb221cbaedae4617e904973e0aacbb4c6b111a2
vitali <v.kostev@gmail.com>
2019-09-24 14:39:47 +0300

    UserMenu__profile

--- src/userMenu.js
+++ src/userMenu.js
@@ -4,8 +4,14 @@ class UserMenu extends React.Component{
     render(){
         return(
             <div className="UserMenu">
-                <p>User menu</p>
+
                 <i class="fa fa-bell fa-2x" aria-hidden="true"></i>
+
+                <div className="UserMenu__profile">
+                    <img alt="user_img" src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"/>
+                    <p>Maria Zvaginceva</p> 
+                </div>
+                                                      
             </div>
         )
     }    
