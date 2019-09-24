82dd91180025690fdbafda1d5eaf4e757705ce0e
max <maxdon2001@gmail.com>
2019-09-24 14:40:36 +0300

    Bell

--- src/userMenu.js
+++ src/userMenu.js
@@ -4,8 +4,8 @@ class UserMenu extends React.Component{
     render(){
         return(
             <div className="UserMenu">
+                <i id="UserMenu__Bell" class="fa fa-bell fa-2x" aria-hidden="true"><div id="circle">2</div></i>
                 <p>User menu</p>
-                <i class="fa fa-bell fa-2x" aria-hidden="true"></i>
             </div>
         )
     }    
