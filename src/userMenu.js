b976f5dbfa915bbd4c59cf80f34ff3a007b8514e
vitali <v.kostev@gmail.com>
2019-09-25 12:37:19 +0300

    CSS, sign up, popup

--- src/userMenu.js
+++ src/userMenu.js
@@ -22,24 +22,41 @@ import React from 'react';
 
 class UserMenu extends React.Component{
     render(){
-        return(
-            <div className="UserMenu">
-                <i id="UserMenu__Bell" class="fa fa-bell fa-2x" aria-hidden="true"><div id="circle">2</div></i>
+        let loggedIn = false;
+        if(loggedIn === true){
+            return(
+                <div className="UserMenu">
+                    <i id="UserMenu__Bell" class="fa fa-bell fa-2x" aria-hidden="true"><div id="circle">2</div></i>
 
-                <div onMouseDown={() => openMenu()} className="UserMenu__profile">
-                    <img class="UserMenu__profile_img" alt="user_img" src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"/>
-                    <div class="dropdown">
-                        <button class="dropbtn">Maria Zvaginceva</button>
-                        <div id="myDropdown" class="dropdown-content">
-                                <a href="#home">Profile</a>
-                                <a href="#about">Edit profile</a>
-                                <span></span>
-                                <a id="logout_btn" href="#login">Logout</a>
+                    <div onMouseDown={() => openMenu()} className="UserMenu__profile">
+                        <img class="UserMenu__profile_img" alt="user_img" src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"/>
+                        <div class="dropdown">
+                            <button class="dropbtn">Maria Zvaginceva</button>
+                            <div id="myDropdown" class="dropdown-content">
+                                    <a href="#profile">Profile</a>
+                                    <a href="#about">Edit profile</a>
+                                    <span></span>
+                                    <a id="logout_btn" href="#login">Logout</a>
+                            </div>
                         </div>
-                    </div>
-                </div>                      
-            </div>
-        )
+                    </div>                      
+                </div>
+            )
+        }else{
+            return(
+                <button className="Sign_in_btn">
+                    <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+                    SIGN IN
+                </button>
+
+                // <div className='popup'>  
+                //     <div className='popup_inner'>  
+                //         <h1>Test</h1>  
+                //         <button>close me</button>
+                //     </div>
+                // </div>
+            )
+        }
     }    
 }
 export default UserMenu
