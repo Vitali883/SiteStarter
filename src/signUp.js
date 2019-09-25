660a72537e99ee7b6c634a77d80d7931985dda29
vitali <v.kostev@gmail.com>
2019-09-25 16:07:05 +0300

    Login form

--- src/signUp.js
+++ src/signUp.js
@@ -8,8 +8,25 @@ function SignUp() {
     return (
         <div className='popup'>
             <div className='popup_inner'>
-                <h1>Test</h1>
-                <button onClick={() => dispatch(closeSignUpPop())}>close me</button>
+                <div id="popUp_Header">
+                    <span></span>
+                    <h1>Sign in</h1>
+                    <button onClick={() => dispatch(closeSignUpPop())}>X</button>
+                </div>
+                <div id="popUp_Content">
+
+                    <label for="uname"><b>Email</b></label><br />                    
+                    <input type="email" id="user-name" name="user-name" placeholder="email" /><br />
+
+                    <label for="uname"><b>Password</b></label><br />
+                    <input type="password" id="password" name="password" placeholder="password" />
+
+                    <br />
+                    <button type="submit">Login</button>
+                </div>
+                <div id="popUp_Footer">
+                    <button><i class="fa fa-user fa-2x" aria-hidden="true"></i> Create account</button>
+                </div>
             </div>
         </div>
     )
