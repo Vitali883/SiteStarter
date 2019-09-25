8b23578fec636d31bf73cc46d55c2c19aa8c4903
max <maxdon2001@gmail.com>
2019-09-25 14:02:41 +0300

    Notification dropdown

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-import { createStore } from 'redux';
+import {createStore} from 'redux';
 import allReducer from './reducers';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 // called when state changed (Redux state)
-function saveToLocalStorage(state){
-    try{
+function saveToLocalStorage(state) {
+    try {
         const serializedState = JSON.stringify(state);
         localStorage.setItem('state', serializedState)
-    }catch(e){
+    } catch (e) {
         console.log(e)
     }
 }
 
 // called on createStore (for example on refresh)
-function loadFromLocalStorage(){
-    try{
+function loadFromLocalStorage() {
+    try {
         const serializedState = localStorage.getItem('state')
-        if(serializedState === null) return undefined
+        if (serializedState === null) 
+            return undefined
         return JSON.parse(serializedState)
-    }catch(e){
+    } catch (e) {
         console.log(e)
         return undefined
     }
@@ -33,19 +33,13 @@ function loadFromLocalStorage(){
 
 const persistedState = loadFromLocalStorage()
 
-let store = createStore(
-    allReducer,
-    persistedState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+let store = createStore(allReducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),);
 
 store.subscribe(() => saveToLocalStorage(store.getState())) // subscribe - if state changed calling the save function
 
 ReactDOM.render(
-    <Provider store = {store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+    <Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
--- src/userMenu.js
+++ src/userMenu.js
@@ -9,10 +9,28 @@ function openMenu() {
         .toggle("show");
 }
 
+function openNotifications() {
+    document
+        .getElementById("myNotifDropdown")
+        .classList
+        .toggle("show");
+}
+
 window
     .addEventListener("click", function (event) {
         console.log(event.target)
-        if (!event.target.matches('.dropbtn') && !event.target.matches('.dropdown') && !event.target.matches('.UserMenu__profile_img') && !event.target.matches('.UserMenu__profile') && !event.target.matches('.UserMenu')) {
+        if (
+            !event.target.matches('.dropbtn') && 
+            !event.target.matches('.dropdown') && 
+            !event.target.matches('.UserMenu__profile_img') && 
+            !event.target.matches('.UserMenu__profile') && 
+            !event.target.matches('.UserMenu') &&
+            !event.target.matches('.dropbtn2') && 
+            !event.target.matches('.dropdown') && 
+            !event.target.matches('.fa') && 
+            !event.target.matches('.UserMenu')
+            ) 
+        {
             var dropdowns = document.getElementsByClassName("dropdown-content");
             var i;
 
@@ -33,8 +51,17 @@ function UserMenu() {
     if (isLogged) {
         return (
             <div className="UserMenu">
-                <i id="UserMenu__Bell" class="fa fa-bell fa-2x" aria-hidden="true">
-                    <div id="circle">2</div>
+                <i id="UserMenu__Bell" class="fa fa-bell fa-2x" aria-hidden="true" onMouseDown={() => openNotifications()}>
+                    <div class="dropdown">
+                        <div class="dropbtn2" id="circle">2</div>
+
+                        <div id="myNotifDropdown" class="dropdown-content">
+                            <a href="#profile">Profile</a>
+                            <a href="#about">Edit profile</a>
+                            <span></span>
+                            <a id="logout_btn" href="#login">Logout</a>
+                        </div>
+                    </div>
                 </i>
 
                 <div onMouseDown={() => openMenu()} className="UserMenu__profile">
@@ -44,6 +71,7 @@ function UserMenu() {
                         src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"/>
                     <div class="dropdown">
                         <button class="dropbtn">Maria Zvaginceva</button>
+
                         <div id="myDropdown" class="dropdown-content">
                             <a href="#profile">Profile</a>
                             <a href="#about">Edit profile</a>
