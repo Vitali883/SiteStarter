644488da64a0759489fcbeaec6d934b7d212a21f
vitali <v.kostev@gmail.com>
2019-09-26 16:47:49 +0300

    Router react

--- src/content.js
+++ src/content.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import About from './about'
+import Home from './home'
 
-class Content extends React.Component{
-    render(){
-        return(
-            <main>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                <h2>awdwawadw</h2>
-                <img src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"></img>
-            </main>
-        )
-    }    
+function Content(){
+    return(
+        <main>
+            <Router>
+                <Switch>
+                    <Route path="/home" component={Home} />
+                    <Route path="/about" component={About} />
+                </Switch>                
+            </Router>
+        </main>
+    )      
 }
 export default Content
--- src/nav.js
+++ src/nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 function Navbar() {
     return(
         <nav className="Navbar">
-            <a href="#home">Для Работадателей</a>
-            <a href="#home">Для Соискателей</a>
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
         </nav>
     )
 }
