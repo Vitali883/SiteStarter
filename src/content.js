989e54bf93464a0c700563dcb892397e2f767f8e
vitali <v.kostev@gmail.com>
2019-09-27 16:34:57 +0300

    Nav bar with langs

--- src/content.js
+++ src/content.js
@@ -3,33 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import About from './about'
 import Home from './home'
 
-import {choosen_language} from './actions';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-function reloadPage(lang){
-    let first_half_url = "http://vps707430.ovh.net:3000/"+lang
 
-    let curr_url = window.location.href
-    let a = curr_url.split('/')
-    let seconds_half_url = "";
-
-    for(let i = 4;i<a.length;i++){
-        seconds_half_url+='/'+a[i];
-    }
-    const full_url = first_half_url+seconds_half_url
-    window.location.href = full_url;
-}
-
-function Content() {    
+function Content() {   
     const curr_lang = useSelector(state => state.userLanguage);
-    const dispatch = useDispatch();
-
     return(
         <main>
-            <button onMouseUp={() => reloadPage('ru')} onClick={() => dispatch(choosen_language('ru'))}>RU</button>
-            <button onMouseUp={() => reloadPage('en')} onClick={() => dispatch(choosen_language('en'))}>EN</button>
-            <button onMouseUp={() => reloadPage('et')} onClick={() => dispatch(choosen_language('et'))}>ET</button>
-
             <Router>
                 <Switch>               
                     <Route path={"/" + curr_lang + "/about"} component={About} />
--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,36 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {choosen_language} from './actions'
+
+function reloadPage(lang){
+    let first_half_url = "http://vps707430.ovh.net:3000/"+lang
+
+    let curr_url = window.location.href
+    let a = curr_url.split('/')
+    let seconds_half_url = "";
+
+    for(let i = 4;i<a.length;i++){
+        seconds_half_url+='/'+a[i];
+    }
+    const full_url = first_half_url+seconds_half_url
+    window.location.href = full_url;
+}
 
 function Navbar() {
     const curr_lang = useSelector(state => state.userLanguage);
+    const dispatch = useDispatch();
 
     return(
         <nav className="Navbar">
-            <a href={"/" + curr_lang + "/home"}>Home</a>
-            <a href={"/" + curr_lang + "/about"}>About</a>
+            <div>
+                <a href={"/" + curr_lang + "/home"}>Home</a>
+                <a href={"/" + curr_lang + "/about"}>About</a>
+            </div>
+            <div>
+                <button onMouseUp={() => reloadPage('ru')} onClick={() => dispatch(choosen_language('ru'))}>RU</button>
+                <button onMouseUp={() => reloadPage('en')} onClick={() => dispatch(choosen_language('en'))}>EN</button>
+                <button onMouseUp={() => reloadPage('et')} onClick={() => dispatch(choosen_language('et'))}>ET</button>
+            </div>
         </nav>
     )
 }
