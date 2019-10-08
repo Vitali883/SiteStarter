d8c91c4c606e290dddd0161dc3d737f9f7c6220f
vitali <v.kostev@gmail.com>
2019-10-08 17:28:40 +0300

    Made class instead of function in LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,53 +1,101 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import siteConfig from '../site-config';
 
 
-function LoginForm() {
-    return(
-        <form className="loginForm">
-            <h2>Login</h2>
-            <label>
-                <b>
-                    <FormattedMessage
-                        id='signin.email'
-                        defaultMessage='Email'
-                    />
-                </b>
-            </label>
-            <br />
-            <input 
-                type='text'
-                name='login'
-                placeholder='email'
-                value=""
-                onChange=""
-            />
-            <br />
-            <label>
-                <b>
-                    <FormattedMessage
-                        id='signin.password'
-                        defaultMessage='Password'
-                    />
-                </b>
-            </label>
-            <br />
-            <input
-                type='password'
-                name='password'
-                placeholder='password'
-                value=""
-                onChange=""
-            />
-            <br />
-            <button type='submit'>
-                <FormattedMessage
-                    id='signin.login'
-                    defaultMessage='Login'
+class LoginForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            login: "",
+            password: "",
+        }
+
+        this.handleChangeState = this.handleChangeState.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+
+        let handleLogin = this.state.login;
+        let handlePassword = this.state.password;
+
+        const data = {
+            'email' : handleLogin,
+            'password' : handlePassword
+        };
+
+        return fetch(`${siteConfig.laravelApiUrl}/api/login`,{
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',                
+            },
+            body: JSON.stringify(data),
+            method : "POST",
+        })
+        .then(r => r.json())
+        .then(data => {
+            if(data.data[1] === 'fail'){
+                alert('Fail')
+            }else{
+                alert('Logged in')
+            }
+        });
+        
+    }
+
+    handleChangeState(event) {
+        const {name, value} = event.target;
+        this.setState({ [name]: value })
+    }
+    render() {
+        return(
+            <form className="loginForm">
+                <h2>Login</h2>
+                <label>
+                    <b>
+                        <FormattedMessage
+                            id='signin.email'
+                            defaultMessage='Email'
+                        />
+                    </b>
+                </label>
+                <br />
+                <input 
+                    type='text'
+                    name='login'
+                    placeholder='email'
+                    value={this.props.login}
+                    onChange={this.handleChangeState}
                 />
-            </button>
-        </form>
-    )
+                <br />
+                <label>
+                    <b>
+                        <FormattedMessage
+                            id='signin.password'
+                            defaultMessage='Password'
+                        />
+                    </b>
+                </label>
+                <br />
+                <input
+                    type='password'
+                    name='password'
+                    placeholder='password'
+                    value={this.props.password}
+                    onChange={this.handleChangeState}
+                />
+                <br />
+                <button type='submit'>
+                    <FormattedMessage
+                        id='signin.login'
+                        defaultMessage='Login'
+                    />
+                </button>
+            </form>
+        )
+    }
 }
 
 export default LoginForm;
--- src/views/LoginAndRegForm.js
+++ src/views/LoginAndRegForm.js
@@ -1,68 +1,16 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 import LoginForm from './../components/LoginForm';
 import Registration from '../components/Registration';
 
-import siteConfig from '../site-config';
 
-
-class LoginAndRegForm extends React.Component {
-
-    constructor() {
-        super();
-        this.state = {
-            login: "",
-            password: "",
-        }
-
-        this.handleChangeState = this.handleChangeState.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleSubmit(event) {
-        event.preventDefault()
-
-        let handleLogin = this.state.login;
-        let handlePassword = this.state.password;
-
-        const data = {
-            'email' : handleLogin,
-            'password' : handlePassword
-        };
-
-        return fetch(`${siteConfig.laravelApiUrl}/api/login`,{
-            headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',                
-            },
-            body: JSON.stringify(data),
-            method : "POST",
-        })
-        .then(r => r.json())
-        .then(data => {
-            if(data.data[1] === 'fail'){
-                alert('Fail')
-            }else{
-                alert('Logged in')
-            }
-        });
-        
-    }
-
-    handleChangeState(event) {
-        const {name, value} = event.target;
-        this.setState({ [name]: value })
-    }
-
-    render() {    
-        return (
-            <main class="loginAndRegisterPage">
-                <LoginForm />
-                <Registration />
-            </main>
-        );
-    }
+function LoginAndRegForm() {
+    return (
+        <main class="loginAndRegisterPage">
+            <LoginForm />
+            <Registration />
+        </main>
+    );
 }
 
 export default LoginAndRegForm;
